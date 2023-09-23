abf1f3ef3e6648ac776f1b8b0682a1838e21cd28
Amit Azulay <amit@tetavi.com>
2023-09-23 23:02:21 +0300

    initial project

---
+++ BulletinBoard/BulletinBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletinBoardApp.swift
+//  BulletinBoard
+//
+//  Created by Amit Azulay on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BulletinBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
